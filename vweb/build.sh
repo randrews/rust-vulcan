0c3a290ce85cfb32b1f85a9274a16b61cd5edaf2
Ross Andrews <ross.andrews@gmail.com>
2023-02-06 12:20:03 -0600

    Basics page

--- vweb/build.sh
+++ vweb/build.sh
@@ -2,4 +2,4 @@
 
 wasm-pack build --target web
 npm run bundle
-cp pkg/vweb_bg.wasm src/index.html src/snippet_emulator_demo.html build
+cp pkg/vweb_bg.wasm src/index.html src/snippet_emulator_demo.html src/basics.html build
