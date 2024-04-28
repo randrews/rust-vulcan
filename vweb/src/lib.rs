c8c5a5d99cc3dfbd7ef865bd780d8f68fb88a81b
Ross Andrews <ross.andrews@gmail.com>
2024-04-28 00:01:15 -0500

    Expose Forge compiler

--- vweb/src/lib.rs
+++ vweb/src/lib.rs
@@ -4,6 +4,7 @@ mod nova_forth;
 mod error;
 
 use wasm_bindgen::prelude::*;
+use forge_core::compiler::build_boot;
 use crate::error::JsVasmError;
 
 #[wasm_bindgen]
@@ -15,4 +16,9 @@ pub fn assemble_snippet(snippet: String) -> Result<Vec<u8>, JsVasmError> {
 pub fn source_map(snippet: String) -> JsValue {
     let source_map = vasm_core::snippet_source_map(snippet.lines().map(String::from)).map_err(|err| JsVasmError::from(err));
     serde_wasm_bindgen::to_value(&source_map).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn compile_forge(src: String) -> Result<String, String> {
+    build_boot(src.as_str()).map(|s| s.join("\n")).map_err(|e| format!("{}", e))
 }
